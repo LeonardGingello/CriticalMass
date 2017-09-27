191cfd2d315c9219e8d586e5fa647fb154e66791
Unknown <leonard.gingello@mymail.champlain.edu>
2017-09-27 18:10:28 -0400

    InitialCommit

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+   
+    public float range = 1f;
+    public UnityEvent grow;
+    public UnityEvent shrink;
+    public Camera fpsCam;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1"))
+        {
+            
+            ShootGrow();
+            grow.Invoke();
+
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+
+            ShootShrink();
+            shrink.Invoke();
+
+        }
+    }
+
+
+    void ShootGrow()
+    {
+        RaycastHit hit;
+
+
+
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range) && hit.collider.gameObject.tag == "ScaleObject")
+        {
+            hit.collider.gameObject.GetComponent<TargetScale>().ScaleGrow();
+            Debug.Log(hit.transform.name);
+
+        }
+
+    
+        else
+        {
+            Debug.Log("Not Hit Skybox");
+
+        }
+
+
+    }
+
+    void ShootShrink()
+    {
+        RaycastHit hit2;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit2, range) && hit2.collider.gameObject.tag == "ScaleObject")
+        {
+            hit2.collider.gameObject.GetComponent<TargetScale>().ScaleShrink();
+            Debug.Log(hit2.transform.name);
+
+        }
+       
+
+        else
+        {
+            Debug.Log("Not Hit Skybox");
+
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/TargetScale.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScale : MonoBehaviour {
+
+
+    public float Grow;
+    public float GrowTime;
+    public float Shrink;
+    public GameObject Self;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void ScaleGrow()
+    {
+        Self.transform.localScale = Vector3.Lerp(Self.transform.localScale, new Vector3(Grow, Grow, Grow), Time.deltaTime * GrowTime);
+        Debug.Log("HOLYSHITTHATWORKED");
+
+    }
+
+    public void ScaleShrink()
+    {
+        Self.transform.localScale = Vector3.Lerp(Self.transform.localScale, new Vector3(Shrink, Shrink, Shrink), Time.deltaTime * GrowTime);
+        Debug.Log("HOLYSHITTHATWORKED");
+
+    }
+}
